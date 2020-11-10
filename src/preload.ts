f8ab5912ab5c21b90081ce4bfa1a8f90562e7f18
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-11-10 18:28:40 +0200

    feat: use webcam

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
@@ -8,5 +8,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
   for (const type of ["chrome", "node", "electron"]) {
     replaceText(`${type}-version`, process.versions[type]);
+  }
+
+  const constraints = { video: { facingMode: "environment" } };
+
+  try {
+    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    const video = document.querySelector("video");
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  } catch (err) {
+    console.error("something went wrong :(", err);
   }
 });
